27b1ca496d354a522ab6dcc66805b01a4ca00a94
Abdullah Qadri <abdullahq@Abdullahs-MacBook-Air-2.local>
2024-12-26 18:38:04 -0800

    Remove all files

--- New Lazeez Payroll Project/src/Main.java
+++
@@ -1,7 +0,0 @@
-public Main() {
-    this.payrollManager = new PayrollManager();
-    String filePath = "/Users/abdullahq/Desktop/Personal Projects CS/New Lazeez Payroll Project/New Lazeez Payroll Project/src/employeeSheet.csv";
-    payrollManager.loadEmployeesFromFile(filePath);
-
-    initializeUI();
-}
